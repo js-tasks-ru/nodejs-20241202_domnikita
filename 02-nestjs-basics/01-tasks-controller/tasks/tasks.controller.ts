fed2b68cd043196d37cb5687188847a025e821a6
Никита Доменикан <domnikita@mail.ru>
2024-12-19 16:36:59 +0300

    implement tasks service and controller

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  NotFoundException,
   Param,
   Patch,
   Post,
@@ -15,17 +16,45 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {}
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 
   @Get(":id")
-  getTaskById(@Param("id") id: string) {}
+  getTaskById(@Param("id") id: string) {
+    const task = this.tasksService.getTaskById(id);
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return task;
+  }
 
   @Post()
-  createTask(@Body() task: Task) {}
+  createTask(@Body() task: Task) {
+    return this.tasksService.createTask(task);
+  }
 
   @Patch(":id")
-  updateTask(@Param("id") id: string, @Body() task: Task) {}
+  updateTask(@Param("id") id: string, @Body() task: Task) {
+    const updated = this.tasksService.updateTask(id, task);
+
+    if (!updated) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return updated;
+  }
 
   @Delete(":id")
-  deleteTask(@Param("id") id: string) {}
+  deleteTask(@Param("id") id: string) {
+    const deleted = this.tasksService.deleteTask(id);
+
+    if (!deleted) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return deleted;
+  }
 }
